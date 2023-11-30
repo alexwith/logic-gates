78878494ef9cf851e084a042248c4fd18c18c6be
Alex <alex@with.priv.no>
2023-11-30 16:17:52 +0100

    Start improving connections

--- src/components/Connection/index.tsx
+++ src/components/Connection/index.tsx
@@ -7,33 +7,15 @@ interface Props {
   active: boolean;
 }
 
+const ORIGIN_PADDING = 25;
+
 export default function Connection({ pos0, pos1, active }: Props) {
   const { x: x0, y: y0 } = pos0;
   const { x: x1, y: y1 } = pos1;
 
   const lineStyle = `stroke-[4px] ${active ? "stroke-red-500" : "stroke-slate-700"}`;
 
-  const create4Line = (x0: number, y0: number, x1: number, y1: number) => {
-    const middleY = (y0 + y1) / 2;
-    return (
-      <>
-        <line
-          className={lineStyle}
-          x1={x0}
-          y1={middleY}
-          x2={x1}
-          y2={middleY}
-          stroke="white"
-          strokeWidth={4}
-          width={4}
-        />
-        <line className={lineStyle} x1={x0} y1={y0} x2={x0} y2={middleY} />
-        <line className={lineStyle} x1={x1} y1={y1} x2={x1} y2={middleY} />
-      </>
-    );
-  };
-
-  const create3Line = (x0: number, y0: number, x1: number, y1: number) => {
+  const createZigLine = (x0: number, y0: number, x1: number, y1: number) => {
     const middleX = (x0 + x1) / 2;
     const minY = Math.min(y0, y1);
     const diffY = Math.abs(y0 - y1);
@@ -47,12 +29,28 @@ export default function Connection({ pos0, pos1, active }: Props) {
     );
   };
 
-  const createLine = () => {
-    if (false) {
-      return create4Line(x0, y0, x1, y1);
-    }
+  const createStapleLine = (x0: number, y0: number, x1: number, y1: number) => {
+    const middleX = (x0 + x1) / 2;
+    const minY = Math.min(y0, y1);
+    const diffY = Math.abs(y0 - y1);
 
-    return create3Line(x0, y0, x1, y1);
+    const endX1 = x0 + ORIGIN_PADDING;
+
+    return (
+      <>
+        <line className={lineStyle} x1={x0} y1={y0} x2={endX1} y2={y0} />
+        <line className={lineStyle} x1={endX1} y1={y0} x2={endX1} y2={y1} />
+        <line className={lineStyle} x1={endX1} y1={y1} x2={x1} y2={y1} />
+      </>
+    );
+  };
+
+  const createLine = () => {
+    /*if (x1 < x0 + ORIGIN_PADDING) {
+      return createStapleLine(x0, y0, x1, y1);
+    }*/
+
+    return createZigLine(x0, y0, x1, y1);
   };
 
   return <>{createLine()}</>;
