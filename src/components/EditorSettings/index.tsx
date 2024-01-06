79147ff10d114b9063256e7ea4b4b1f4d0d75d00
Alex <alex@with.priv.no>
2024-01-06 12:09:52 +0100

    chore: Remove zoom settings

--- src/components/EditorSettings/index.tsx
+++ src/components/EditorSettings/index.tsx
@@ -1,5 +1,4 @@
 import { BsFillGrid3X3GapFill as GridIcon } from "react-icons/bs";
-import { CgZoomIn as ZoomInIcon, CgZoomOut as ZoomOutIcon } from "react-icons/cg";
 import { EditorState, useEditorStore } from "../../store";
 
 export default function EditorSettings() {
@@ -15,8 +14,6 @@ export default function EditorSettings() {
   return (
     <div className="absolute flex space-x-3 justify-center items-center bg-zinc-800 p-2 right-0 bottom-0 m-2 rounded-md [&>*]:hover:cursor-pointer">
       <GridIcon size={20} onClick={handleToggleGrid} />
-      <ZoomInIcon size={20} />
-      <ZoomOutIcon size={20} />
     </div>
   );
 }
