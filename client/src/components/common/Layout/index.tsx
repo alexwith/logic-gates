d55e8bff6c92761ce43afda34fe228c9b23e188d
Alex <alex@with.priv.no>
2024-07-21 19:11:32 +0200

    feat: make clicking logo go to home route

--- client/src/components/common/Layout/index.tsx
+++ client/src/components/common/Layout/index.tsx
@@ -1,13 +1,21 @@
 import MiniProfile from "../../profile/MiniProfile";
 import { ToastContainer } from "react-toastify";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 export function Layout() {
+  const routerNavigate = useNavigate();
+
+  const handleHomeClick = () => {
+    routerNavigate("/");
+  };
+
   return (
     <>
       <div className="flex flex-col items-center">
         <div className="flex h-20 justify-between items-center w-full px-10 max-w-[1500px]">
-          <h1 className="font-black text-3xl">Logic Gates</h1>
+          <h1 className="font-black text-3xl hover:cursor-pointer" onClick={handleHomeClick}>
+            Logic Gates
+          </h1>
           <MiniProfile />
         </div>
         <Outlet />
