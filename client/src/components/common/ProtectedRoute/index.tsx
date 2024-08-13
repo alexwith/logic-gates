b0490f4d34436d7cbe976db7fe3815f4e7ab8f7f
Alex <alex@with.priv.no>
2024-08-13 20:47:55 +0200

    fix: protected routes would often fail even when you were logged in

--- client/src/components/common/ProtectedRoute/index.tsx
+++ client/src/components/common/ProtectedRoute/index.tsx
@@ -2,7 +2,11 @@ import { Navigate } from "react-router-dom";
 import { useUser } from "../../../hooks/useUser";
 
 export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
-  const { isLoggedIn } = useUser();
+  const { isLoggedIn, isLoading } = useUser();
+  if (isLoading) {
+    return null;
+  }
+
   if (!isLoggedIn) {
     return <Navigate to="/" replace />;
   }
