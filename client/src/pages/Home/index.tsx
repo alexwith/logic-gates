cbcb713569ecdbdad69fd284905878eceb0cdfb1
Alex <alex@with.priv.no>
2024-08-24 23:51:41 +0200

    feat: clean up home page

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,9 +1,26 @@
+import { Link } from "react-router-dom";
 import Discovery from "../../components/common/Discovery";
 
 export default function Home() {
   return (
-    <div>
-      <Discovery />
+    <div className="flex flex-col mt-10 items-center">
+      <h1 className="font-bold text-5xl text-center">
+        <span className="text-violet-500">Build</span>,{" "}
+        <span className="text-green-400">simulate</span> and{" "}
+        <span className="text-sky-400">share</span> circuits
+      </h1>
+      <p className="text-white-300 text-xl text-center max-w-2xl p-2">
+        Easily create and simulate logic gate circuits, then share your designs for collaboration
+        and learning.
+      </p>
+      <Link to="/playground">
+        <div className="bg-violet-500 px-4 py-2 font-bold text-xl rounded-md m-6 shadow-[0_0px_20px_0px_rgba(139,92,246,0.9)] hover:cursor-pointer">
+          Try playground
+        </div>
+      </Link>
+      <div className="w-fit pt-10">
+        <Discovery />
+      </div>
     </div>
   );
 }
